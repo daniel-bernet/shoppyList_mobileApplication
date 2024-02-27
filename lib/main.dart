da7ee28928c5a6cdd344aa2df9377e67da291ee9
Daniel Bernet <bernet.daniel2@gmail.com>
2024-02-27 21:08:19 +0100

    Revert "dependencies resolution"
    This reverts commit 3f5debf4331fd303ff67623c6d551f380c321bc1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
