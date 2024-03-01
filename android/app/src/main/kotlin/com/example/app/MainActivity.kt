623d8c02276dcc61d35e03ba85843fe48f2e42ba
Daniel Bernet <bernet.daniel2@gmail.com>
2024-03-01 23:31:23 +0100

    firebase connection

--- android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.app
+package com.shoppylist.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
